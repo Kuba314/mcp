5cb46fa944dff4d2cf7a246283c1a17fcfa4a851
Kuba314 <35199722+Kuba314@users.noreply.github.com>
2022-01-03 02:39:51 +0100

    add on_statistics

---
+++ src/handlers/play/on_statistics.c
@@ -0,0 +1,25 @@
+#include "packet_handler.h"
+
+int on_statistics(unionstream_t *stream)
+{
+    int32_t count;
+    if(stream_read_varint(stream, &count)) {
+        return 1;
+    }
+
+    debug("player", "stats: {");
+    for(int32_t i = 0; i < count; i++) {
+        string_t *field_name = stream_read_string(stream);
+        int32_t field_value;
+        if(stream_read_varint(stream, &field_value)) {
+            return 1;
+        }
+
+        debug("player", "   \"%s\": %d,", field_name->s, field_value);
+
+        string_free(field_name);
+    }
+    debug("player", "}");
+
+    return 0;
+}
